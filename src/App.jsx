c2943003df99e96957a8c1d4172d1483b7452fa3
Abdikani971 <abdikanimohamed00971@gmail.com>
2025-04-19 22:10:35 +0300

    feat: integrate Header component into App for improved navigation

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import LoginPage from "./pages/login";
 import SignUpPage from "./pages/singup";
 import { AuthProvider } from "./providers/auth-context";
 import UnAuthenticatedRoute from "./components/un-authenticated";
+import { Header } from "./components/header/header";
 
 function App() {
   return (
     <AuthProvider>
+      <Header />
       <main>
         <Routes>
           <Route index element={<HomePage />} />
