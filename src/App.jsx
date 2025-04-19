b0c0dfaacdce1d6a5e98afc5e85798d7b5baffe5
Abdikani971 <abdikanimohamed00971@gmail.com>
2025-04-19 18:58:32 +0300

    feat: wrap App component with AuthProvider for user authentication context

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,19 @@ import { Route, Routes } from "react-router";
 import HomePage from "./pages/home";
 import LoginPage from "./pages/login";
 import SignUpPage from "./pages/singup";
+import { AuthProvider } from "./providers/auth-context";
 
 function App() {
   return (
-    <main>
-      <Routes>
-        <Route index element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignUpPage />} />
-      </Routes>
-    </main>
+    <AuthProvider>
+      <main>
+        <Routes>
+          <Route index element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+        </Routes>
+      </main>
+    </AuthProvider>
   );
 }
 
