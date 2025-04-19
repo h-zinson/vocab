5233f71d28e76b10301078082448726b08467fd8
Abdikani971 <abdikanimohamed00971@gmail.com>
2025-04-19 18:14:43 +0300

    feat: add route for login page in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router";
 import HomePage from "./pages/home";
+import LoginPage from "./pages/login";
 
 function App() {
   return (
     <main>
       <Routes>
         <Route index element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </main>
   );
