890cf972acfec9912f377c90cbdda8023ce3e88d
Abdikani971 <abdikanimohamed00971@gmail.com>
2025-04-20 12:25:29 +0300

    feat: add CardDecorator and CustomerLogos components for enhanced UI card design and customer showcase

---
+++ src/components/features/card-dec.jsx
@@ -0,0 +1,15 @@
+export const CardDecorator = ({ children }) => (
+  <div className="relative mx-auto size-36 duration-300 [--color-border:color-mix(in_oklab,var(--color-zinc-950)10%,transparent)] group-hover:[--color-border:color-mix(in_oklab,var(--color-zinc-950)20%,transparent)] dark:[--color-border:color-mix(in_oklab,var(--color-white)15%,transparent)] dark:group-hover:bg-white/5 dark:group-hover:[--color-border:color-mix(in_oklab,var(--color-white)20%,transparent)]">
+    <div
+      aria-hidden
+      className="absolute inset-0 bg-[linear-gradient(to_right,var(--color-border)_1px,transparent_1px),linear-gradient(to_bottom,var(--color-border)_1px,transparent_1px)] bg-[size:24px_24px]"
+    />
+    <div
+      aria-hidden
+      className="bg-radial to-background absolute inset-0 from-transparent to-75%"
+    />
+    <div className="dark:bg-background absolute inset-0 m-auto flex size-12 items-center justify-center rounded-xl border-l border-t bg-white shadow-sm transition-transform duration-300 group-hover:scale-110">
+      {children}
+    </div>
+  </div>
+);
